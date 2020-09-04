da6191e35f6493d44135c0c6e1e15614318a7a62
redeniuseva <redeniuseva@urbandaleschools.com>
2020-09-04 11:29:33 -0500

    answers complete

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -5,17 +5,18 @@ Sign your name: Evan Redenius
 
 Define Forking: coping a document and making it local for you to work on
 
-Define Cloning: 
+Define Cloning: taking the document and making a copy into github
 
-Define Branching:
+Define Branching: taking the document from github and putting it into python
 
-Define Committing: 
+Define Committing: putting a hard save on what you've been doing
 
 Define Merging: taking branch documents and murging them to the master
 
-Define Pushing:
+Define Pushing: taking the document from python and pushing it to github
 
-Define Pull Request:
+Define Pull Request: taking the documents that came from python and ask the reciver if they will take your changes
+from our document and add the changes to theirs
 
 
 '''
